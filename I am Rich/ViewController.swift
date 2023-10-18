8e388b1e32af4a8d4c4e03545ddaee992ca12abc
Gustavo Lima <136847064+limafgustavo@users.noreply.github.com>
2023-10-18 16:04:38 -0300

    feat: Initial commit.

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gustavo Lima on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+        
+    }
+
+
+}
+
